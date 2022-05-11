6933c895a04ea0b28aa9382b3f8c2cc25c585986
James Kao <james.l.kao@gmail.com>
2022-05-11 11:37:57 -0700

    Log window.selection() on focus

--- static/lexical-component.js
+++ static/lexical-component.js
@@ -1,26 +1,42 @@
 export default class LexicalComponent extends HTMLElement {
-
   constructor() {
     super();
-    const shadowRoot = this.attachShadow({mode: 'open'});
+    const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(LexicalComponent.template.content.cloneNode(true));
   }
 
   async connectedCallback() {
-    const $contentEditableElement = this.shadowRoot.querySelector('#editor');
-    // NOTE: This breaks encapsulation but ensures we're using the same logic across document and webcomponent.
-    const editor = initializeEditorToElement($contentEditableElement);
-    console.log({
-      msg: "[lexical-component] connectedCallback",
-      editor,
-      document,
-      $contentEditableElement,
+    const $contentEditableElement = this.shadowRoot.querySelector("#editor");
+    $contentEditableElement.addEventListener("focus", () => {
+      /*
+        Logs the following:
+          anchorNode: div#container
+          anchorOffset: 11
+          baseNode: div#container
+          baseOffset: 11
+          extentNode: div#container
+          extentOffset: 11
+          focusNode: div#container
+          focusOffset: 11
+          isCollapsed: true
+          rangeCount: 1
+          type: "Range"
+      */
+      console.log("SELECTION ON FOCUS", window.getSelection());
     });
+    // NOTE: This breaks encapsulation but ensures we're using the same logic across document and webcomponent.
+    // const editor = initializeEditorToElement($contentEditableElement);
+    // console.log({
+    //   msg: "[lexical-component] connectedCallback",
+    //   editor,
+    //   document,
+    //   $contentEditableElement,
+    // });
   }
 
   static template = (() => {
-    var template = document.createElement('template');
-    template.innerHTML = /* html */`
+    var template = document.createElement("template");
+    template.innerHTML = /* html */ `
       <style>
         :host {
           display: flex;
@@ -53,6 +69,5 @@ export default class LexicalComponent extends HTMLElement {
       <div id=editor contenteditable>Dummy text</div>
     `;
     return template;
-  })()
-
+  })();
 }
